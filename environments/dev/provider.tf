82f0211aa9269871387991801ee4d43f88e4042b
Tran Vi <tranvix.work@gmail.com>
2026-06-03 14:14:15 +0700

    refactor: disable recovery window for database secrets and add commented-out backend configuration

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -15,3 +15,16 @@ provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket       = "vib-wineapp-terraform-state"
+#     key          = "environments/dev/terraform.tfstate"
+#     region       = "ap-southeast-1"
+#     encrypt      = true
+#     use_lockfile = true
+#   }
+# }
+
+# aws secretsmanager delete-secret --secret-id wineapp-mongodb-password --force-delete-without-recovery
+# aws secretsmanager delete-secret --secret-id wineapp-mongodb-connection-string --force-delete-without-recovery
